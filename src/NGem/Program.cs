ad9c8566fd01e315334c9f5a802a961b973624b0
Philip Proplesch <philip@proplesch.de>
2010-08-22 13:42:49 +0200

    Updated usage.

--- src/NGem/Program.cs
+++ src/NGem/Program.cs
@@ -68,9 +68,9 @@ namespace devplex
         
         static void PrintUsage()
         {
-            Console.WriteLine("gem install {packagename}");
+            Console.WriteLine("ngem install {packagename}");
             Console.WriteLine(" or");
-            Console.WriteLine("gem make {pathtolib} {manufacturer} {library}");
+            Console.WriteLine("ngem make {pathtolib} {manufacturer} {library}");
         }
     }
 }
