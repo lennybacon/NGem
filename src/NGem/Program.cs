5d2e4e21d19a58ff1c8b2adbce1e27cfc0637ea7
Daniel Fisher (lennybacon.com) <info@lennybacon.com>
2010-08-23 23:54:15 +0200

    fixed a type in the PrintUsage method.

--- src/NGem/Program.cs
+++ src/NGem/Program.cs
@@ -129,7 +129,7 @@ namespace devplex
         {
             Console.WriteLine("gem install {packagename}");
             Console.WriteLine(" or");
-            Console.WriteLine("gem make {pathtolib} {library} {manufaturer} {library}");
+            Console.WriteLine("gem make {pathtolib} {manufacturer} {library}");
             Console.WriteLine(string.Empty);
             Console.WriteLine("Hit [ENTER] to quit...");
             Console.ReadLine();
